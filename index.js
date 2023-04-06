8a4fea99dc5856a3d3c8938a19a01ccb5bbf993f
Gilberto Garcia <gilbertogjn@gmail.com>
2023-04-06 09:59:25 -0400

    PWA Prompt Add
    Implementation of custom PWA prompt.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ getItemsFromLocalStorage()
 
 addButtonEl.addEventListener('click', addItemOnLocalStorage)
 inputFieldEl.addEventListener('keypress', function handleKeyPress(event) {
-    if(event.key === 'Enter') {
+    if (event.key === 'Enter') {
         addItemOnLocalStorage()
     }
 })
@@ -70,8 +70,39 @@ function nextId() {
     return lastId
 }
 
-window.onload = function() {
+window.onload = function () {
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/service-worker.js");
+        navigator.serviceWorker.register("/service-worker.js")
     }
-  };
+}
+
+
+//Install PWA Prompt
+let deferredPrompt
+let setupButton
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    deferredPrompt = e
+    console.log("beforeinstallprompt fired")
+    if (setupButton == undefined) {
+        setupButton = document.getElementById("install-button")
+    }
+    setupButton.style.display = "inline"
+    setupButton.disabled = false
+})
+
+function installApp() {
+    deferredPrompt.prompt()
+    setupButton.disabled = true
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
+            if(choiceResult.outcome === 'accepted') {
+                console.log('PWA setup accepted')
+                setupButton.style.display = 'none'
+            } else {
+                console.log('PWA setup rejected')
+            }
+            deferredPrompt = null
+        })
+}
